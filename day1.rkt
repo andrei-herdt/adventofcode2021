418d02a73d6128746eea870a4d6edcc3ec7cfbd2
Andrei Herdt <andrei.herdt@tra.ai>
2021-12-17 15:29:07 +0000

    final solution day1.txt

--- day1.rkt
+++ day1.rkt
@@ -4,7 +4,4 @@
   (for/list ([x v][xs (cdr v)])
             (- xs x)))
 
-(for/fold ([sum 0])
-          ([inc (adjacent_diff - (file->list "day1.txt"))])
-          (cond 
-            [(> inc 0) (+ sum 1) sum]))
+(count positive? (adjacent_diff - (file->list "day1.txt")))
