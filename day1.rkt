1b6123bd07355586eda75bf2a98f3985e2646295
Andrei Herdt <andrei.herdt@tra.ai>
2021-12-17 15:22:25 +0000

    second attempt

--- day1.rkt
+++ day1.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
-(for/fold ([sum_increments 0][previous 0])
-     ([line (file->lines "day1.txt")])
-     (values 
-       (+ sum_increments (string->number line))
-       (cond [(> previous (string->number line)) previous (string->number line)])))
+(define (adjacent_diff f v)
+  (for/list ([x v][xs (cdr v)])
+            (- xs x)))
+
+(for/fold ([sum 0])
+          ([inc (adjacent_diff - (file->list "day1.txt"))])
+          (cond 
+            [(> inc 0) (+ sum 1) sum]))
